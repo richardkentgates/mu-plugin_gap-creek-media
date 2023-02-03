bf005c2d3829b154f37bbf595cc55c83712f3161
Richard Gates <mail@richardkentgates.com>
2023-02-03 14:08:21 -0600

    Create gap-creek-media.php

---
+++ gap-creek-media.php
@@ -0,0 +1,26 @@
+/*
+Plugin Name: Gap Creek Media
+*/
+
+/* Section: A */
+
+add_filter('site_status_tests', function (array $tests) { 
+  unset($tests['direct']['theme_version'],
+        $tests['direct']['plugin_version'],
+        $tests['direct']['scheduled_events'],
+        $tests['direct']['persistent_object_cache'], 
+        $tests['async']['page_cache']);
+  return $tests;
+}, 10, 1);
+
+/* Section: B */
+
+add_filter( 'wp_mail_smtp_tasks_admin_hide_as_menu', '__return_false' );
+
+/* Section: C */
+
+add_filter( 'auto_update_plugin', '__return_true' );
+
+/* Section: D */
+
+add_filter( 'auto_update_theme', '__return_true' );
