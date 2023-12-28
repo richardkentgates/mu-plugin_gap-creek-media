fa696d3a27a23ca8143cc21f45189ba790ce45e3
Richard Gates <mail@richardkentgates.com>
2023-12-27 23:18:48 -0600

    Update gap-creek-media.php
    Removed filters

--- gap-creek-media.php
+++ gap-creek-media.php
@@ -1,9 +1,6 @@
 /*
 Plugin Name: Gap Creek Media
 */
-
-/* Section: A */
-
 add_filter('site_status_tests', function (array $tests) { 
   unset($tests['direct']['theme_version'],
         $tests['direct']['plugin_version'],
@@ -12,15 +9,6 @@ add_filter('site_status_tests', function (array $tests) {
         $tests['async']['page_cache']);
   return $tests;
 }, 10, 1);
-
-/* Section: B */
-
-add_filter( 'wp_mail_smtp_tasks_admin_hide_as_menu', '__return_false' );
-
-/* Section: C */
-
-add_filter( 'auto_update_plugin', '__return_true' );
-
-/* Section: D */
-
-add_filter( 'auto_update_theme', '__return_true' );
+add_shortcode( 'gcm_sitetitle', function (){
+   return get_bloginfo( 'name' );
+}, 10, 2);
