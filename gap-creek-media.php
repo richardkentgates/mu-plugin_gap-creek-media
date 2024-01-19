b8105a27f7c85d9a094fa2acf034afc772465eaa
Richard Gates <mail@richardkentgates.com>
2024-01-19 03:10:30 -0600

    Update gap-creek-media.php
    Moved update info here

--- gap-creek-media.php
+++ gap-creek-media.php
@@ -1,6 +1,12 @@
 /*
 Plugin Name: Gap Creek Media
 */
+add_filter('auto_update_plugin', '__return_true');
+add_filter('auto_update_theme', '__return_true');
+add_filter('auto_update_core', '__return_true');
+add_filter('auto_plugin_update_send_email', '__return_false');
+add_filter('auto_theme_update_send_email', '__return_false');
+add_filter('auto_core_update_send_email', '__return_false');
 add_filter('site_status_tests', function (array $tests) { 
   unset($tests['direct']['theme_version'],
         $tests['direct']['plugin_version'],
